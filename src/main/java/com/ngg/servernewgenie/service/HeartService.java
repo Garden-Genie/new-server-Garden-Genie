7b72c5bdf36ae8ff09e3a4bfc31d857feeec5ec7
MyeongJi_Shin <89967069+tlsaudwl@users.noreply.github.com>
2023-11-27 01:31:54 +0900

    Update HeartService.java

--- src/main/java/com/ngg/servernewgenie/service/HeartService.java
+++ src/main/java/com/ngg/servernewgenie/service/HeartService.java
@@ -3,8 +3,11 @@ package com.ngg.servernewgenie.service;
 import com.ngg.servernewgenie.domain.Heart;
 import com.ngg.servernewgenie.domain.Story;
 import com.ngg.servernewgenie.domain.User;
-import com.ngg.servernewgenie.dto.HeartDTO;
+import com.ngg.servernewgenie.dto.HeartRequestDTO;
+import com.ngg.servernewgenie.dto.HeartResponseDTO;
 import com.ngg.servernewgenie.repository.HeartRepository;
+import com.ngg.servernewgenie.repository.StoryRepository;
+import com.ngg.servernewgenie.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -17,30 +20,53 @@ import java.util.stream.Collectors;
 @Transactional
 public class HeartService {
 
+    private final UserRepository userRepository;
+    private final StoryRepository storyRepository;
+    private final HeartRepository heartRepository;
+
     @Autowired
-    private HeartRepository heartRepository;
+    public HeartService(UserRepository userRepository, StoryRepository storyRepository, HeartRepository heartRepository) {
+        this.userRepository = userRepository;
+        this.storyRepository = storyRepository;
+        this.heartRepository = heartRepository;
+    }
 
-    public HeartDTO addLike(Long userId, Long storyId) {
-        User liker = new User(userId);
-        Story story = new Story(storyId);
+    public HeartResponseDTO addLike(HeartRequestDTO requestDTO) {
+        User liker = userRepository.findById(requestDTO.getUserId())
+                .orElseThrow(() -> new RuntimeException("사용자가 존재하지 않습니다."));
 
-        Heart existingLike = heartRepository.findByLikerAndStory_id(liker, story);
+        Story story = storyRepository.findById(requestDTO.getStoryId())
+                .orElseThrow(() -> new RuntimeException("스토리가 존재하지 않습니다."));
+
+//        User liker = new User(requestDTO.getUserId());
+//        Story story = new Story();
+//        story.setStory_id(requestDTO.getStoryId());
+
+
+        Heart existingLike = heartRepository.findByLikerAndStory(liker, story);
 
         if (existingLike == null) {
             Heart newLike = new Heart(null, liker, story);
             Heart savedLike = heartRepository.save(newLike);
 
-            return new HeartDTO(savedLike.getH_id(), userId, storyId);
+            return new HeartResponseDTO(savedLike.getH_id(), requestDTO.getUserId(), requestDTO.getStoryId());
         } else {
             throw new RuntimeException("이미 좋아요를 눌렀습니다.");
         }
     }
 
-    public void deleteLike(Long userId, Long storyId) {
-        User liker = new User(userId);
-        Story story = new Story(storyId);
+    public void deleteLike(HeartRequestDTO requestDTO) {
+        User liker = userRepository.findById(requestDTO.getUserId())
+                .orElseThrow(() -> new RuntimeException("사용자가 존재하지 않습니다."));
 
-        Heart existingLike = heartRepository.findByLikerAndStory_id(liker, story);
+        Story story = storyRepository.findById(requestDTO.getStoryId())
+                .orElseThrow(() -> new RuntimeException("스토리가 존재하지 않습니다."));
+
+//        User liker = new User(requestDTO.getUserId());
+//        Story story = new Story();
+//        story.setStory_id(requestDTO.getStoryId());
+
+        Heart existingLike = heartRepository.findByLikerAndStory(liker, story);
 
         if (existingLike != null) {
             heartRepository.delete(existingLike);
@@ -49,19 +75,25 @@ public class HeartService {
         }
     }
 
+    public List<HeartResponseDTO> getLikedStoriesByUser(Long userId) {
+        User user = userRepository.findById(userId)
+                .orElseThrow(() -> new RuntimeException("사용자가 존재하지 않습니다."));
+
+        List<Heart> hearts = heartRepository.findByLiker(user);
+
+        return hearts.stream()
+                .map(heart -> new HeartResponseDTO(heart.getH_id(), userId, heart.getStory().getStory_id()))
+                .collect(Collectors.toList());
+    }
+
     public int getLikeCountByStory(Long storyId) {
-        Story story = new Story(storyId);
-        List<Heart> hearts = heartRepository.findByStory_id(story);
+        Story story = storyRepository.findById(storyId)
+                .orElseThrow(() -> new RuntimeException("스토리가 존재하지 않습니다."));
+
+        List<Heart> hearts = heartRepository.findByStory(story);
 
         return hearts.size();
     }
 
-    public List<HeartDTO> getLikedStoriesByUser(Long userId) {
-        User user = new User(userId);
-        List<Heart> hearts = heartRepository.findByLiker(user);
 
-        return hearts.stream()
-                .map(heart -> new HeartDTO(heart.getH_id(), userId, heart.getStory_id().getStory_id()))
-                .collect(Collectors.toList());
-    }
 }
